faf0eeeee354456f3fcb6fed864a2e808a6fae20
LegoMan28 <craftersofbedrock@gmail.com>
2014-08-24 13:44:01 -0700

    Created v0.1
    Created the program within about 3 minutes to complete v0.1  More may be added soon.

---
+++ PythonProgram.py
@@ -0,0 +1,7 @@
+name = raw_input("What is your name?")
+day = raw_input("How is your day going?")
+great = ("That's great.")
+
+print "So yo name is %s, and yo day is going %s?" \
+% (name, day)
+print great.upper()
